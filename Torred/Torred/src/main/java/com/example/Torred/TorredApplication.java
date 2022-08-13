98323434e38f3d2cd9db8c5fb5e284371770e434
apoorvkhare259 <aman.khare64@gmail.com>
2022-08-13 10:39:21 +0530

    added code

---
+++ Torred/Torred/src/main/java/com/example/Torred/TorredApplication.java
@@ -0,0 +1,13 @@
+package com.example.Torred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorredApplication.class, args);
+	}
+
+}
---
+++ Torred/Torred/src/test/java/com/example/Torred/TorredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Torred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
